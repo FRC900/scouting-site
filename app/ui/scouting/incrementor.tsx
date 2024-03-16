c532e5b787cb1395bdaee638fc1d006bf4b3e5f5
robbiestoffel <robbiestoffel@gmail.com>
2024-03-16 18:17:28 -0400

    stand scouting form progress. Incrementors=broken

--- app/ui/scouting/incrementor.tsx
+++ app/ui/scouting/incrementor.tsx
@@ -1,31 +1,50 @@
-'use client';
+"use client";
 
 import { useState } from "react";
+import { MinusIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 export default function Incrementor({ name }: { name: string }) {
-  
-  
-  
-  function handleClick() {
+	const [count, setCount] = useState(0);
 
-  }
+	function increment() {
+		setCount(count - 1);
+	}
 
-  return (
-    <div>
-      <button>
+	function decrement() {
+		setCount(count + 1);
+	}
 
-      </button>
-      <input 
-        id={ name }
-        name={ name }
-        type="number"
-        placeholder="0"
-        aria-describedby={name + "-error"}
-        className="p-2 border border-slate-950 rounded-md text-slate-950"
-      />
-      <button>
+	function updateValue(e: any) {
+		setCount(e);
+	}
 
-      </button>
-    </div>
-  )
+	return (
+		<div className="flex flex-row gap-1">
+			<button
+				onClick={increment}
+				className="px-5 py-2 bg-white rounded border-2 border-slate-950"
+			>
+				<p className="text-3xl text-slate-950">-</p>
+			</button>
+			<input
+				id={name}
+				name={name}
+				type="number"
+				step="1"
+				value={count}
+				onChange={(event) => {
+					updateValue(event.target.value);
+				}}
+				aria-describedby={name + "-error"}
+				className="p-2 border-2 border-slate-950 rounded-md text-slate-950 w-12"
+			/>
+			<button
+				onClick={decrement}
+				className="px-5 py-2 bg-slate-700 rounded border-2 border-slate-950"
+			>
+				<p className="text-3xl text-slate-950">+</p>
+			</button>
+		</div>
+	);
 }
--- app/ui/scouting/stand-form.tsx
+++ app/ui/scouting/stand-form.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
 import { TeamField } from "@/app/lib/definitions";
+import Incrementor from "@/app/ui/scouting/incrementor";
+import { INSPECT_MAX_BYTES } from "buffer";
 
 export default function StandForm({ teams }: { teams: TeamField[] }) {
 	return (
-		<form>
-			<div className="flex flex-row gap-4 text-slate-200 text-xl rounded-3xl p-5 w-max">
+		<form className="text-slate-200 flex flex-col gap-2">
+			<div className="flex flex-row gap-4 text-xl rounded-3xl p-5 w-max">
 				<div className="flex flex-col gap-4">
 					<label className="p-2">Match Number:</label>
 					<input
@@ -65,18 +66,60 @@ export default function StandForm({ teams }: { teams: TeamField[] }) {
 					/>
 				</div>
 			</div>
-      <div>
-        <p><strong>Auto</strong></p>
-        <div>
-          <label>Speaker</label>
-          <div>
-            <label>Scored</label>
-            <div>
-              
-            </div>
-          </div>
-        </div>
-      </div>
+			<div className="flex flex-row gap-5">
+				<p className="w-12">
+					<strong>Auto</strong>
+				</p>
+				<div>
+					<label>Speaker</label>
+					<div>
+						<label>Scored</label>
+						<Incrementor name="auto-speaker-scored" />
+					</div>
+					<div>
+						<label>Missed</label>
+						<Incrementor name="auto-speaker-missed" />
+					</div>
+				</div>
+				<div>
+					<label>Amp</label>
+					<div>
+						<label>Scored</label>
+						<Incrementor name="auto-amp-scored" />
+					</div>
+					<div>
+						<label>Missed</label>
+						<Incrementor name="auto-amp-missed" />
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-row gap-5">
+				<p className="w-12">
+					<strong>Teleop</strong>
+				</p>
+				<div>
+					<label>Speaker</label>
+					<div>
+						<label>Scored</label>
+						<Incrementor name="teleop-speaker-scored" />
+					</div>
+					<div>
+						<label>Missed</label>
+						<Incrementor name="teleop-speaker-missed" />
+					</div>
+				</div>
+				<div>
+					<label>Amp</label>
+					<div>
+						<label>Scored</label>
+						<Incrementor name="teleop-amp-scored" />
+					</div>
+					<div>
+						<label>Missed</label>
+						<Incrementor name="teleop-amp-missed" />
+					</div>
+				</div>
+			</div>
 		</form>
 	);
 }
