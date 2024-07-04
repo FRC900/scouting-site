75e5ad4a4ccd778980ad6914991b8c0aee7a1d45
robbiestoffel <robbiestoffel@gmail.com>
2024-07-04 11:14:02 -0400

    dont worry im mentally ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
 import type { Metadata } from "next";
-import { Ubuntu } from "next/font/google";
-import  "@/components/ui/globals.css";
+import { Roboto } from "next/font/google";
+import "@/components/ui/globals.css";
 import Header from "@/components/header/header";
 
 export const metadata: Metadata = {
-  title: "Scouting",
-  description: "The Zebracorn's scouting system for First Robotics Competitions.",
+	title: "Scouting",
+	description:
+		"The Zebracorn's scouting system for First Robotics Competitions.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className='${Ubuntu.className} bg-zinc-900 p-6 text-lg'>
-        <div className="flex flex-col h-screen overflow-auto">
-          <div className="w-full flex-none">
-            <Header />
-          </div>
-          <div className="w-max mx-auto">{children}</div>
-        </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+    <body className={`flex flex-col h-screen overflow-auto ${Roboto} bg-zinc-900 p-6 text-lg`}>
+				<div className="w-full flex-none">
+					<Header />
+				</div>
+				<div className="flex-grow w-max mx-auto">{children}</div>
+			</body>
+		</html>
+	);
 }
 
 // bg-gradient-to-tl from-slate-950 to-slate-800
