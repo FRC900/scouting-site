67932d68ee4e7af96b4720dd693b0dd02b4b7e17
robbiestoffel <robbiestoffel@gmail.com>
2024-10-15 12:40:22 -0400

    styling is hard

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,19 +1,26 @@
-import { Paper, Title } from "@mantine/core";
+'use client';
+
+import { Burger, Container, Group, Title } from "@mantine/core";
+import classes from "./Header.module.css";
 import NavLinks from "./NavLinks";
-import styles from './Header.module.css'
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
 
 export default function Header() {
+	const [opened, { toggle }] = useDisclosure(false);
+	const items = NavLinks();
+
 	return (
-		<Paper className={styles.header}>x``
-			{/* <Image
-					src="/Logo-Slate.png"
-					width={32}
-					height={32}
-					alt="Zebracorns Logo"
-					unoptimized
-				/> */} 
-			<Title order={3}>Zebracorn Scouting</Title>
-			<NavLinks />
-		</Paper>
+		<header className={classes.header}>
+			<Container size="md">
+				<div className={classes.inner}>
+					<Link href="/" className={classes.link}><Title order={3}>Zebracorn Scouting</Title></Link>
+					<Group gap={5} visibleFrom="sm">
+						{items}
+					</Group>
+					<Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+				</div>
+			</Container>
+		</header>
 	);
 }
