2718f7828fb1ce6ac0cde244b1f9436369958428
robbiestoffel <robbiestoffel@gmail.com>
2025-02-15 12:30:36 -0500

    auth works but cant get user info which is problamatic...ok for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Welcome } from "../components/Welcome/Welcome";
+import Dashboard from "../components/Dashboard/Dashboard";
+import { useSession } from 'next-auth/react'
 
 export default function HomePage() {
+  // const { data: session } = useSession()
+
   return (
     <>
-      <Welcome />
+      {false ? <Dashboard/> : <Welcome/>}
     </>
   );
 }
