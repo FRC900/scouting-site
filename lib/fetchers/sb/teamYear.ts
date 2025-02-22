fc218f62e01376d568c718d53f4eb5b5386fd25c
robbiestoffel <robbiestoffel@gmail.com>
2025-02-22 12:38:58 -0500

    added error handling

--- lib/fetchers/sb/teamYear.ts
+++ lib/fetchers/sb/teamYear.ts
@@ -3,11 +3,24 @@ import { getSBHeaders } from "../getHeaders";
 
 export default async function teamYear(team: number) {
   const headers = getSBHeaders();
-  const apiRes = await fetch(
-    `https://api.statbotics.io/v3/team_year/${team}/${year}`,
-    {
-      headers,
+  try {
+    const apiRes = await fetch(
+      `https://api.statbotics.io/v3/team_year/${team}/${year}`,
+      {
+        headers,
+      }
+    ).then((res) => res.json());
+    return apiRes;
+  } catch (error) {
+    console.log(error)
+    return {
+      "name": "(statbotics down)",
+      "epa": {
+        "total_points": { 
+          "mean": 0,
+          "sd": 0
+        },
+      }
     }
-  ).then((res) => res.json());
-  return apiRes;
+  }
 }
