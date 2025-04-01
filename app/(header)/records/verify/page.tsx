573b898736d5ce5cefdfcbdad9017835ce21573b
robbiestoffel <robbiestoffel@gmail.com>
2025-04-01 16:03:45 -0400

    force dynamic

--- app/(header)/records/verify/page.tsx
+++ app/(header)/records/verify/page.tsx
@@ -4,24 +4,7 @@ import { VerificationErrors } from "../../../../lib/definitions";
 import verify from "../../../../lib/verify";
 import { unstable_cache } from "next/cache";
 
-/**
- * Level 0:
- *
- * total score
- *
- * Level 1:
- *
- * total auto points
- * total teleop points
- * minorfouls
- * majorfouls
- *
- * Level 2:
- *
- * autol1, autol2, autol3, autol4
- * teleopl1, teleopl2, teleopl3, teleopl4
- * teleopnet, teleopprocessor
- */
+export const dynamic = 'force-dynamic'
 
 const cached_verify = unstable_cache(
   async () => {
